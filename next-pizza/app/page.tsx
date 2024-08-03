0e6d8655fbf2989cb0e4c7515f8eb97409fdd64e
a.bukhtatiy <buhtatyalexander90@gmail.com>
2024-08-03 22:12:45 +0300

    add categories

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,3 +1,14 @@
+import {Title} from '@/components/shared/title';
+import {Container} from '@/components/shared/container';
+import {Categories} from '@/components/shared/categories';
+
 export default function Home() {
-  return <h1>Главная</h1>;
+    return (
+        <>
+            <Container className="mt-10">
+                <Title text="Все пиццы" size="lg" className="font-extrabold"></Title>
+                <Categories></Categories>
+            </Container>
+        </>
+    );
 }
