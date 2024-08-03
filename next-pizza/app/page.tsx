9b852c971cb0cd087ebba16160ab5a2d7f70ff7d
a.bukhtatiy <buhtatyalexander90@gmail.com>
2024-08-03 22:29:52 +0300

    make top-bar markup

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,14 +1,14 @@
 import {Title} from '@/components/shared/title';
 import {Container} from '@/components/shared/container';
-import {Categories} from '@/components/shared/categories';
+import {TopBar} from '@/components/shared/top-bar';
 
 export default function Home() {
     return (
         <>
             <Container className="mt-10">
                 <Title text="Все пиццы" size="lg" className="font-extrabold"></Title>
-                <Categories></Categories>
             </Container>
+            <TopBar></TopBar>
         </>
     );
 }
---
+++ next-pizza/components/shared/top-bar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {cn} from '@/lib/utils';
+import {Categories} from '@/components/shared/categories';
+import {SortPopup} from '@/components/shared/sort-popup';
+import {Container} from '@/components/shared/container';
+
+interface Props {
+    categories: any[]; // TODO: добавить интерфейс
+    className?: string;
+}
+
+export const TopBar: React.FC<Props> = ({ categories, className }) => {
+    return (
+        <div className={cn(
+            'sticky top-0 bg-white py-5 z-10',
+            false && 'shadow-lg shadow-black/5', // TODO: добавить условие при котором будем отображать тень
+            className
+        )}>
+            <Container className="flex items-center justify-between ">
+                <Categories items={categories} />
+                <SortPopup />
+            </Container>
+        </div>
+    );
+};
