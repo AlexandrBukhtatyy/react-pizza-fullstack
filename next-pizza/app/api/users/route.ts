9bb5c3c55ca5c692a1d065245600406653ea7a97
a.bukhtatiy <buhtatyalexander90@gmail.com>
2024-08-08 22:17:16 +0300

    add api's

--- next-pizza/app/api/users/route.ts
+++ next-pizza/app/api/users/route.ts
@@ -1,15 +1,19 @@
-import {NextRequest, NextResponse} from 'next/server';
-import {prisma} from '@/prisma/prisma-client';
+import { prisma } from '@/prisma/prisma-client';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
-    const users = await prisma.user.findMany();
-    return NextResponse.json(users);
+  // SELECT * FROM users WHERE email = 'emasd'
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
 }
 
 export async function POST(req: NextRequest) {
-    const data = await req.json();
-    const user = await prisma.user.create({
-        data
-    });
-    return NextResponse.json(user);
+  const data = await req.json();
+
+  const user = await prisma.user.create({
+    data,
+  });
+
+  return NextResponse.json(user);
 }
