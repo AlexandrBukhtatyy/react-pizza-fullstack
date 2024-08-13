ee18b85ae814524506a43cace6dbf2943c7213af
a.bukhtatiy <buhtatyalexander90@gmail.com>
2024-08-13 20:50:37 +0300

    add behaviour to price slider

--- next-pizza/components/shared/filters.tsx
+++ next-pizza/components/shared/filters.tsx
@@ -10,14 +10,29 @@ import {CheckboxFiltersGroup} from '@/components/shared/checkbox-filters-group';
 import {RadioGroup, RadioGroupItem} from '@/components/ui/radio-group';
 import {Label} from '@/components/ui/label';
 import {useFilterIngredients} from '@/hooks/use-FilterIngredients';
+import {valueOf} from 'postcss';
 
 interface Props {
     className?: string;
 }
 
+interface PriceProps {
+    priceFrom: number;
+    priceTo: number;
+}
+
 export const Filters: React.FC<React.PropsWithChildren<Props>> = ({className}) => {
     const {ingredients, loading, selectedIds, onAddId} = useFilterIngredients();
     const items = ingredients.map(i => ({text: i.name, value: String(i.id)}));
+    const [prices, setPrice] = React.useState<PriceProps>({
+        priceFrom: 0, priceTo: 1000
+    });
+    const updatePrice = (name: keyof PriceProps, value: number) => {
+        setPrice({
+            ...prices,
+            [name]: value
+        })
+    };
 
     return (
         <div className={className}>
@@ -30,10 +45,17 @@ export const Filters: React.FC<React.PropsWithChildren<Props>> = ({className}) =
             <div className="mt-5 border-t border-y-neutral-100 py-6 pb-7">
                 <p className="font-bold mb-3">Цена от и до</p>
                 <div className="flex gap-3 mb-5">
-                    <Input type="number" min={0} max={1000} placeholder="0" defaultValue={0}/>
-                    <Input type="number" min={100} max={1000} placeholder="1000"/>
+                    <Input type="number" min={0} max={1000} placeholder="0"
+                           value={String(prices.priceFrom)}
+                           onChange={(price) => updatePrice('priceFrom', Number(price.target.value))}/>
+                    <Input type="number" min={100} max={1000} placeholder="1000"
+                           value={String(prices.priceTo)}
+                           onChange={(price) => updatePrice('priceTo', Number(price.target.value))}/>
                 </div>
-                <RangeSlider min={0} max={1000} step={10} value={[0, 1000]}/>
+                <RangeSlider min={0} max={1000} step={10}
+                             value={[Number(prices.priceFrom), Number(prices.priceTo)]}
+                             onValueChange={([priceFrom, priceTo]) => setPrice({priceFrom: Number(priceFrom), priceTo: Number(priceTo)})}
+                />
             </div>
 
             <div className="mt-5 border-t border-y-neutral-100 py-6 pb-7">
